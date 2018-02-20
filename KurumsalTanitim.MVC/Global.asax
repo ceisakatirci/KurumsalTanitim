67f7eeb7391079178555926b986f4187826d9673
ceisakatirci <ce.isakatirci@gmail.com>
2018-02-20 20:04:14 +0300

    Add project files.

---
+++ KurumsalTanitim.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KurumsalTanitim.MVC.MvcApplication" Language="C#" %>
---
+++ KurumsalTanitim.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KurumsalTanitim.MVC.Startup))]
+namespace KurumsalTanitim.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
